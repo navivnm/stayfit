0cd30bdda0622f90ff069581aee286fccc7fe32f
Naveen Vijay <naveenvijay@Naveens-Mac.local>
2017-07-31 17:50:14 +0530

    Initial Commit

---
+++ Stay Fit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stay Fit
+//
+//  Created by Naveen Vijay on 31/07/17.
+//  Copyright © 2017 Naveen Vijay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
